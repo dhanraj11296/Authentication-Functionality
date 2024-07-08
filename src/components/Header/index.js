f03472a24aaff76ce103bf2ab23364a17d43e583
dhanraj <rvsanjayraj@gmail.com>
2024-07-08 12:39:28 +0000

    Solution

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+const Header = () => (
+  <div className="header">
+    <ul className="navMenu">
+      <li>
+        <Link to="/" className="navLink">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/about" className="navLink">
+          About
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
+export default Header
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,8 @@
+// Write your JS code here
+import './index.css'
+const NotFound = () => (
+  <div className="notFound">
+    <h1>not Found</h1>
+  </div>
+)
+export default NotFound
